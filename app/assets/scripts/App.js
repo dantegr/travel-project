f3ab67e4cd6475b2aa2598c6c56e1b48b512440c
Pantelis <tampantelis@gmail.com>
2019-01-20 16:07:53 +0200

    Finished mobile menu css

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,3 @@
-var $ = require('jquery');
-function person(name, favcolor){
-	console.log("Hello my name is" + name + "and my favorite color is" + favcolor + ".");
-}
+import MobileMenu from './modules/MobileMenu';
 
-alert("abc 321");
-
-person("John Dow","blue");
-person("Jane Smith","green");
+var mobileMenu = new MobileMenu();
