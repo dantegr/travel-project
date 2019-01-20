3f87633a21f40dc6b1495d78e430eb7c7d2fe98e
Pantelis <tampantelis@gmail.com>
2019-01-20 14:49:11 +0200

    intergrted  webpack and babel

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+var $ = require('jquery');
+function person(name, favcolor){
+	console.log("Hello my name is" + name + "and my favorite color is" + favcolor + ".");
+}
+
+alert("abc 321");
+
+person("John Dow","blue");
+person("Jane Smith","green");
